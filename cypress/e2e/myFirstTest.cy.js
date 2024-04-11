6159431b803647adf9880c73131125f1dd0c39a6
Agnes Iradukunda <kundaaggy@gmail.com>
2024-04-11 23:14:52 +0200

    add the first test

---
+++ cypress/e2e/myFirstTest.cy.js
@@ -0,0 +1,11 @@
+describe('This is my first test', ()=> {
+    it('Verify the title-positive test', ()=> {
+        cy.visit("https://opensource-demo.orangehrmlive.com/")
+        cy.title().should('eq', 'OrangeHRM')
+    })
+
+    it('Verify the title-negative test', ()=> {
+        cy.visit("https://opensource-demo.orangehrmlive.com/")
+        cy.title().should('eq', 'OrangeHRM123')
+    })
+})
